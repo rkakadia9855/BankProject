0236a7369b65c532be0008e0ff69889e32208ff1
johnyy603 <44475350+johnyy603@users.noreply.github.com>
2020-10-06 13:48:36 -0400

    Update Checking.java

--- src/Checking.java
+++ src/Checking.java
@@ -1,5 +1,32 @@
 public class Checking extends Account {
   
   private boolean directDeposit;
+	
+	public Checking(Profile holder, double balance, Date dateOpen) {
+		super(holder, balance, dateOpen);
+	}
+	
+	public boolean isDirectDeposit() {
+		return directDeposit;
+	}
+	
+	public void setDirectDeposit(boolean directDeposit) {
+		this.directDeposit = directDeposit;
+	}
+
+	@Override 
+	public double monthlyInterest() {
+		double interest = 0.005/12;
+		return interest;
+	}
+
+	@Override
+	public double monthlyFee() {
+		double fee = 0;
+		if(!directDeposit || super.getBalance() < 1500) {
+			fee = 25;
+		}
+		return fee;
+	}
 
 }
