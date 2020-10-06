96afbb75647d3ffd45f79f49a37066f732dbc90e
johnyy603 <44475350+johnyy603@users.noreply.github.com>
2020-10-06 13:49:28 -0400

    Update MoneyMarket.java

--- src/MoneyMarket.java
+++ src/MoneyMarket.java
@@ -2,4 +2,31 @@ public class MoneyMarket extends Account {
   
   private int withdrawals;
 
+	public MoneyMarket(Profile holder, double balance, Date dateOpen) {
+		super(holder, balance, dateOpen);
+		
+	}
+	
+	public int getWithdrawals() {
+		return withdrawals;
+	}
+	public void setWithdrawals(int withdrawals) {
+		this.withdrawals = withdrawals;
+	}
+
+	@Override
+	public double monthlyInterest() {
+		double interest = 0.0065/12;
+		return interest;
+	}
+
+	@Override
+	public double monthlyFee() {
+		double fee = 0;
+		if(super.getBalance() < 2500 || withdrawals > 6 ) {
+			fee = 12;
+		}
+		return fee;
+	}
+
 }
