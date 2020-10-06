299aa676a940bae13d058354ea1bedf5f1bca0fb
johnyy603 <44475350+johnyy603@users.noreply.github.com>
2020-10-06 13:50:16 -0400

    Update Savings.java

--- src/Savings.java
+++ src/Savings.java
@@ -2,4 +2,39 @@ public class Savings extends Account {
   
   private boolean isLoyal;
 
+	public Savings(Profile holder, double balance, Date dateOpen) {
+		super(holder, balance, dateOpen);
+		
+	}
+
+	
+	public boolean isLoyal() {
+		return isLoyal;
+	}
+	
+	public void setLoyal(boolean isLoyal) {
+		this.isLoyal = isLoyal;
+	}
+
+	@Override
+	public double monthlyInterest() {
+		double interest = 0;
+		
+		if(isLoyal) {
+			interest = 0.0035/12;
+		}else {
+			interest = 0.0025/12;
+		}
+		return interest;
+	}
+
+	@Override
+	public double monthlyFee() {
+		double fee = 0;
+		if(super.getBalance() < 300) {
+			fee = 5;
+		}
+		return fee;
+	}
+
 }
